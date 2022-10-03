0c3cc62f8264e274316b4260f6372adf350f8d25
Tufahel <tufahel96@gmail.com>
2022-10-03 20:11:49 +0600

    ruby way, day 1

--- day1.rb
+++ day1.rb
@@ -1,13 +1,9 @@
 # How many numbers are greater than five
 
 def how_many_numbers_are_greater_than number, array
-    count = 0
-    for item in array
-        if item > 5
-            count = count + 1
-        end
+    array.count do |item|
+        item>5
     end
-    return count
 end
 
 array = [1,3,4,6,8]
