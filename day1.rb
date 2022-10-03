714fbd27258a354f1b1cf1a9b0b0ebfc0b6b9166
Tufahel <tufahel96@gmail.com>
2022-10-03 20:08:21 +0600

    using method, day1

--- day1.rb
+++ day1.rb
@@ -1,13 +1,15 @@
 # How many numbers are greater than five
 
-array = [1,3,4,6,8]
-number = 5
-count = 0
-
-for item in array
-    if item > 5
-        count = count + 1
+def how_many_numbers_are_greater_than number, array
+    count = 0
+    for item in array
+        if item > 5
+            count = count + 1
+        end
     end
+    return count
 end
 
-puts count
+array = [1,3,4,6,8]
+number = 5
+puts how_many_numbers_are_greater_than(5, array)
